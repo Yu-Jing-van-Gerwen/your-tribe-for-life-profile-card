006afdbdd3f435b29c44985f3986a6a0a933c8a0
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-05 12:58:40 +0200

    de button moet nog gestyled worden en oplossen errors

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,181 +1,188 @@
 <script>
-  export let data
+    export let data
 
-  import linkedin from '$lib/images/linkedin.png';
-  import imagefrom from '$lib/images/image.jpg';
-  import t from '$lib/images/image.jpg';
-  import githublogo from '$lib/images/github-logo.png';
+    import linkedin from '$lib/images/linkedin.png';
+    import imagefrom from '$lib/images/image.jpg';
+    import t from '$lib/images/image.jpg';
+    import githublogo from '$lib/images/github-logo.png';
 
 </script>
 
 <main>
-  <div class="grid-container">
+    <div class="grid-container">
 
-    <h1>{data.person.name}  {data.person.prefix}  {data.person.surname}</h1>
-    <img class="image" src="{imagefrom}"  alt="Dit is mijn foto" />
-    <section class="aboutmesection">
+        <h1>{data.person.name}  {data.person.prefix}  {data.person.surname}</h1>
+        <img class="image" src="{imagefrom}" alt="Dit is mijn foto"/>
+        <section class="aboutmesection">
 
-      <h2 class="about-me text">Over mij</h2>
-      <p class="text"> Leeftijd: 20</p>
-      <p class="bio text" > Bio:{data.person.bio}  </p>
-<!--      <p class="vooropleiding text">Mijn vooropleiding:<a href="https://www.regiocollege.nl/mbo-opleiding/softwaredeveloper">-->
-<!--        {data.person.custom} </a></p>-->
-      <p class="squad text">Op dit moment doe ik de opleiding frontend design en developemnt en zit ik in mijn  afstudeerjaar</p>
-    </section>
+            <h2 class="about-me">Over mij</h2>
+            <p class=""> Leeftijd: 20</p>
+            <p class=" "> Bio:{data.person.bio}  </p>
+            <!--      <p class="vooropleiding ">Mijn vooropleiding:<a href="https://www.regiocollege.nl/mbo-opleiding/softwaredeveloper">-->
+            <!--        {data.person.custom} </a></p>-->
+            <p class=" ">Op dit moment doe ik de opleiding frontend design en developemnt en zit ik in mijn
+                afstudeerjaar</p>
+        </section>
 
-    <section class="socialmedia"><!--social-media-->
-      <h2>Social media</h2>
-      <ul>
-        <li>
-          <a href="https://github.com/yujing-student">
-            <!--						<img src="/images/github-logo.png" width="40px">-->
-            <img src={githublogo} alt="GitHub" width="40px"/>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/yu-jing-van-gerwen-a959431b7/">
-            <img src={linkedin} alt="GitHub" width="40px"/>
-          </a>
-        </li>
-      </ul>
-    </section>
-    <button>druk voor meer info</button>
+        <section class="socialmedia"><!--social-media-->
+            <h2>Social media</h2>
+            <ul class="icons-grid">
+                <li>
+                    <a href="https://github.com/yujing-student">
+                        <!--						<img src="/images/github-logo.png" width="40px">-->
+                        <img src={githublogo} alt="GitHub" width="40px"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/yu-jing-van-gerwen-a959431b7/">
+                        <img src={linkedin} alt="GitHub" width="40px"/>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <button>druk voor meer info</button>
 
-  </div>
+    </div>
 </main>
 
 <style>
 
 
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: open sans, Arial, serif;
-    line-height: 1.5;
-  }
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: open sans, Arial, serif;
+        line-height: 1.5;
+    }
 
-  .grid-container {
-    border-radius: 1em;
-    background: #00ff92;
-    background: linear-gradient(0deg, rgba(0, 239, 255, 1) 40%, #4fdd51 100%);
-    box-shadow: 0 0 20px 20px rgba(0, 0, 0, .1); /*ronde hoeken met een schadwu*/
-    max-width: 450px;
-
-  }
-
-  @media  (max-width: 705px) {
     .grid-container {
-      max-width: 450px;
-      background-color: #ffffff;
-      flex-direction: column;
-      display: flex;
-      padding-left: 2em;
-      padding-right: 2em;
+        border-radius: 1em;
+        background: #00ff92;
+        background: linear-gradient(0deg, rgba(0, 239, 255, 1) 40%, #4fdd51 100%);
+        box-shadow: 0 0 20px 20px rgba(0, 0, 0, .1); /*ronde hoeken met een schadwu*/
+        max-width: 450px;
+
     }
 
-    .image {
-      width: 217px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      text-align: center;
-    }
+    @media (max-width: 705px) {
+        .grid-container {
+            max-width: 450px;
+            background-color: #ffffff;
+            flex-direction: column;
+            display: flex;
+            padding-left: 2em;
+            padding-right: 2em;
+        }
 
-  }
-
-
-
-  h1 {
-    color: #000000;
-    font-size: 2em;
-  }
-
-  .about-me {
-    font-size: 1.5em;
-  }
-
-  section {
-    display: flex;
-    flex-direction: column;
-  }
-
-  p {
-    line-height: 1.5;
-  }
-
-
-  a {
-    text-decoration: none;
-    color: black;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-
-  ul {
-    grid-template-columns: 1fr 1fr;
-
-  }
-
-  li {
-    list-style: none;
-  }
-
-
-  /*ipad en desktop*/
-  @media (min-width: 705px) {
-
-    main{
-      display: flex;
-      align-items: center;
-    }
-
-    .grid-container{
-      display: grid;
-
-
-      grid-template-rows: auto auto auto auto;
-      grid-template-columns: auto auto auto;
-      max-width: 600px;
+        .image {
+            width: 265px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
 
     }
 
 
-    h1{
-      grid-column-start: 1;
-      grid-column-end: 4;
-      grid-row-start: 1;
+    h1 {
+        color: #000000;
+        font-size: 2em;
     }
 
-
-    .image {
-      grid-row-start: 2;
-      grid-column-start: 1;
-      width: 250px;
-      display: grid;
-      object-fit: cover;
-
+    .about-me {
+        font-size: 1.5em;
     }
-    .aboutmesection{
-      grid-row-start: 2;
-      grid-row-end:3 ;
-      grid-column-start: 2;
-      grid-column-end: 4;
-      display: grid;
-    }
-    .socialmedia{
-      grid-row: 3;
-      grid-column: 3;
-    }
-
 
     section {
-      display: block;
-
+        display: flex;
+        flex-direction: column;
     }
 
-  }
+    p {
+        line-height: 1.5;
+    }
+
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+
+    li {
+        list-style: none;
+    }
+    .icons-grid{
+        display: grid;
+        grid-template-rows: auto;
+        grid-template-columns: 4em 1fr;
+
+        padding: 1em;
+    }
+
+
+    /*ipad en desktop*/
+    @media (min-width: 705px) {
+
+        main {
+            display: flex;
+            align-items: center;
+        }
+
+        .grid-container {
+            display: grid;
+
+
+            grid-template-rows: auto auto auto auto;
+            grid-template-columns: auto auto auto;
+            max-width: 600px;
+            gap: 2em;
+            padding: 2em;
+
+        }
+
+
+        h1 {
+            grid-column-start: 1;
+            grid-column-end: 4;
+            grid-row-start: 1;
+        }
+
+
+        .image {
+            grid-row-start: 2;
+            grid-column-start: 1;
+            width: 250px;
+            display: grid;
+            object-fit: cover;
+
+        }
+
+        .aboutmesection {
+            grid-row-start: 2;
+            grid-row-end: 3;
+            grid-column-start: 2;
+            grid-column-end: 4;
+            display: grid;
+        }
+
+        .socialmedia {
+            grid-row: 3;
+            grid-column: 2;
+        }
+
+
+        section {
+            display: block;
+
+        }
+
+    }
 
 </style>
