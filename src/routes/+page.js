d7b5c13aa1ac617a96105520d5e68c235536791e
yujing <medischdossier03@gmail.com>
2024-09-05 15:57:04 +0200

    fixen hmepage is niet gleukt

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,1 +1,10 @@
-export let csr = false
+import getDirectusInstance from '$lib/directus';
+import { readItems } from '@directus/sdk';
+export async function load({ fetch }) {
+    const directus = getDirectusInstance(fetch);
+    return {
+        persons: await directus.request(readItems('person')),
+    };
+}
+
+export let csr = false;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,11 @@
 <script>
   export let data
-  function checkAvatarImage (avatar) {
-    if (avatar.includes('https')) {
-      return true
-    } 
 
-    return false
-  }
 </script>
 
 <main>
   <h1>Squad 1D</h1>
-
-  <ul>
-    {#each data.persons as person}
-      <li>
-        <a href="/{person.id}">
-          <span class="avatar">
-            {#if person.avatar.length > 0 && checkAvatarImage (person.avatar)}
-              <img src="{person.avatar}" alt="Avatar van {person.name}">
-            {:else if person.avatar.length > 0 || checkAvatarImage (person.avatar)}
-              {person.avatar}
-            {:else}
-              <img src="https://robohash.org/mail@ashallendesign.co.uk" alt="Avatar van {person.name}">
-            {/if}
-          </span>
-
-          {person.name}
-        </a>
-      </li>
-    {/each}
-  </ul>
+  <h2>{data.persons[2].name}</h2>
 
 </main>
 
