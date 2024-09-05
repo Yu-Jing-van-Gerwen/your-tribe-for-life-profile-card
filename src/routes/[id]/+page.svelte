fdd89c6a5c65d287d63d88fa96cd76901e2a6225
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-05 11:06:05 +0200

    data opgehaald uit database

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let data
+
+
+</script>
+
+<main>
+  <h1>{data.person.name}  {data.person.prefix}      {data.person.surname}</h1>
+  <ul>
+
+    <li>
+      {data.person.website}
+    </li>
+<!--    todo de custom moet geparsed worden-->
+    <li>
+      {data.person.custom}
+    </li>
+  </ul>
+
+</main>
+
+<style>
+  
+
+</style>
