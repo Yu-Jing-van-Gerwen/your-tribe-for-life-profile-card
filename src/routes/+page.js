009da87da0f935181f015ff88e6be4680c52576f
yujing <medischdossier03@gmail.com>
2024-09-09 11:25:25 +0200

    internal server error is weg

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,2 @@
-import getDirectusInstance from '$lib/directus';
-import { readItems } from '@directus/sdk';
-export async function load({ fetch }) {
-	const directus = getDirectusInstance(fetch);
-	return {
-
-		person: await directus.request(readItems('person')),
-	};
-}
+export let csr = false
 
