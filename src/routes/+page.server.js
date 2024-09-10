4d7dd70430363e33f2bc51d702330b0400b0ccfe
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-10 14:41:58 +0200

    onnodige files weggehaald

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,9 @@
 import fetchJson from "$lib/fetch-json.js"
 
 export async function load() {
-	// const url = 'https://fdnd.directus.app/items/person/?filter={"squad_id":5}'
+	// const url = 'https://fdnd.directus.app/items/person/?filter={"squad_id":3}'
 	const url = 'https://fdnd.directus.app/items/person/9'
-	// mijn eigen url https://fdnd.directus.app/items/person/55
-
 	const persons = await fetchJson(url)
-
 	return {
 		persons: persons.data
 	}
